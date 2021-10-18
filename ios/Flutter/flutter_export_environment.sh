3d7303670bae670bb1f5818ef3e2c5dcdfb9f770
jevoned <48shield@gmail.com>
2021-10-18 18:44:24 +0700

    add flutter

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jevonedmund/Documents/flutter-chat-demo/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jevonedmund/Documents/flutter-chat-demo"
+export "FLUTTER_ROOT=/Users/jevonedmund/Documents/bucinn/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jevonedmund/Documents/bucinn"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
